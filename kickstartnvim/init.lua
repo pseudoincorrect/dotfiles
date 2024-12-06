a0404b33fd68a49e4f998de32a8b379ffbb81d4c
Maxime Clement <m.clement@tadaweb.com>
2024-12-06 18:37:03 +0100

    [vim] update config leader

--- kickstartnvim/init.lua
+++ kickstartnvim/init.lua
@@ -186,10 +186,10 @@ vim.opt.autoread = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<leader>cd', vim.diagnostic.setloclist, { desc = 'Open [D]iagnostic quickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, wgich
 -- is not what someone will guess without a bit more experience.
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
@@ -218,7 +218,7 @@ vim.api.nvim_set_keymap('n', '<C-s>', ':w<CR>', { noremap = true, silent = true,
 vim.api.nvim_set_keymap('i', '<C-s>', '<Esc>:w<CR>', { noremap = true, silent = true, desc = 'Save the current file' })
 
 -- Open a terminal in vertical split
-vim.keymap.set('n', '<leader>ot', ':vert term<CR>', { noremap = true, silent = true, desc = 'Open a terminal in vsplit' })
+vim.keymap.set('n', '<leader>o', ':vert term<CR>', { noremap = true, silent = true, desc = '[O]pen a terminal in vsplit' })
 
 -- Yanky keybindings
 vim.keymap.set({ 'n', 'x' }, 'p', '<Plug>(YankyPutAfter)')
@@ -229,8 +229,8 @@ vim.keymap.set('n', '<c-p>', '<Plug>(YankyPreviousEntry)')
 vim.keymap.set('n', '<c-n>', '<Plug>(YankyNextEntry)')
 
 -- NeoTree
-vim.keymap.set('n', '<leader>e', '<Esc>:Neotree toggle<CR>', { noremap = true, desc = 'Open NeoTree files explorer' })
-vim.keymap.set('n', '<leader>eb', '<Esc>:Neotree toggle source=buffers<CR>', { noremap = true, desc = 'Open NeoTree buffers explorer' })
+vim.keymap.set('n', '<leader>ee', '<Esc>:Neotree toggle<CR>', { noremap = true, desc = 'Open NeoTree files [E]xplor[E]r' })
+vim.keymap.set('n', '<leader>eb', '<Esc>:Neotree toggle source=buffers<CR>', { noremap = true, desc = 'Open NeoTree [E]xplore [B]uffers' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
@@ -359,9 +359,10 @@ require('lazy').setup({
         { '<leader>c', group = '[C]ode', mode = { 'n', 'x' } },
         { '<leader>r', group = '[R]ename' },
         { '<leader>s', group = '[S]earch' },
-        { '<leader>t', group = '[T]oggle' },
-        { '<leader>o', group = '[O]pen' },
         { '<leader>h', group = '[H]arpoon' },
+        { '<leader>d', group = '[D]ebug' },
+        { '<leader>r', group = 'Su[R]round' },
+        { '<leader>e', group = 'N[E]otree' },
       },
     },
   },
@@ -449,40 +450,27 @@ require('lazy').setup({
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>sm', builtin.marks, { desc = '[S]earch [M]arks' })
       vim.keymap.set('n', '<leader>sp', builtin.builtin, { desc = '[S]earch select Telesco[P]e' })
-      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<leader><leader>', function()
-        builtin.buffers { path_display = { 'truncate' }, sort_lastused = true }
-      end, { desc = '[F]ind existing buffers' })
       vim.keymap.set('n', '<leader>sc', builtin.command_history, { desc = '[S] Find existing [C]ommands' })
       vim.keymap.set('n', '<leader>st', builtin.colorscheme, { desc = '[S]earch [T]hemes' })
       vim.keymap.set('n', '<leader>sj', builtin.jumplist, { desc = '[S] Find [J]umplist' })
-      vim.keymap.set('n', '<leader>su', '<cmd>Telescope undo<cr>', { desc = '[S] Find [U]undo' })
-      vim.keymap.set('n', '<leader>sb', builtin.current_buffer_fuzzy_find, { desc = '[S]earch [B]uffer for string' })
 
-      -- Slightly advanced example of overriding default behavior and theme
-      vim.keymap.set('n', '<leader>/', function()
-        -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-        builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-          winblend = 10,
-          previewer = false,
-        })
-      end, { desc = '[/] Fuzzily search in current buffer' })
+      vim.keymap.set('n', 'U', '<cmd>Telescope undo<cr>', { desc = 'Find [U]ndo' })
 
-      -- It's also possible to pass additional configuration options.
-      --  See `:help telescope.builtin.live_grep()` for information about particular keys
-      vim.keymap.set('n', '<leader>s/', function()
-        builtin.live_grep {
-          grep_open_files = true,
-          prompt_title = 'Live Grep in Open Files',
-        }
-      end, { desc = '[S]earch [/] in Open Files' })
+      -- Search files (by name)
+      vim.keymap.set('n', '<S-\t>', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '\t', function()
+        builtin.buffers { path_display = { 'truncate' }, sort_lastused = true }
+      end, { desc = 'Find existing buffers' })
+
+      -- Search word in files
+      vim.keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find, { desc = 'Grep Buffer' })
+      vim.keymap.set('n', '<leader>?', builtin.live_grep, { desc = 'Grep Repo' })
+      vim.keymap.set('n', '<leader>*', builtin.grep_string, { desc = 'Search current word' })
 
       -- Shortcut for searching your Neovim configuration files
       vim.keymap.set('n', '<leader>sn', function()
@@ -603,7 +591,7 @@ require('lazy').setup({
 
           -- Rename the variable under your cursor.
           --  Most Language Servers support renaming across files, etc.
-          map('<leader>rn', vim.lsp.buf.rename, '[R]e[N]ame')
+          map('<leader>cr', vim.lsp.buf.rename, '[C]ode [R]ename')
 
           -- Execute a code action, usually your cursor needs to be on top of an error
           -- or a suggestion from your LSP for this to activate.
@@ -939,13 +927,13 @@ require('lazy').setup({
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup {
         mappings = {
-          add = 'ysa', -- Add surrounding in Normal and Visual modes
-          delete = 'ysd', -- Delete surrounding
-          find = 'ysf', -- Find surrounding (to the right)
-          find_left = 'ysF', -- Find surrounding (to the left)
-          highlight = 'ysh', -- Highlight surrounding
-          replace = 'ysr', -- Replace surrounding
-          update_n_lines = 'ysn', -- Update `n_lines`
+          add = '<leader>ra', -- Add surrounding in Normal and Visual modes
+          delete = '<leader>rd', -- Delete surrounding
+          find = '<leader>rf', -- Find surrounding (to the right)
+          find_left = '<leader>rF', -- Find surrounding (to the left)
+          highlight = '<leader>rh', -- Highlight surrounding
+          replace = '<leader>rr', -- Replace surrounding
+          update_n_lines = '<leader>rg', -- Update `n_lines`
         },
       }
 
--- kickstartnvim/lua/custom/plugins/mini-move.lua
+++ kickstartnvim/lua/custom/plugins/mini-move.lua
@@ -2,6 +2,18 @@ return {
   'echasnovski/mini.move',
   version = '*',
   config = function()
-    require('mini.move').setup()
+    require('mini.move').setup {
+      -- Customize the keybindings here
+      mappings = {
+        left = '<C-S-h>', -- Move selection left with Alt+h
+        right = '<C-S-l>', -- Move selection right with Alt+l
+        down = '<C-S-j>', -- Move selection down with Alt+j
+        up = '<C-S-k>', -- Move selection up with Alt+k
+        line_left = '<C-S-H>', -- Move current line left
+        line_right = '<C-S-L>', -- Move current line right
+        line_down = '<C-S-J>', -- Move current line down
+        line_up = '<C-S-K>', -- Move current line up
+      },
+    }
   end,
 }
