3b7a11fe6832075ce59270a86200224e3710fa46
Maxime Clement <m.clement@tadaweb.com>
2025-07-20 23:10:49 +0200

    [nvim] clean by ai

--- kickstartnvim/lua/custom/plugins/monokai.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'loctvl842/monokai-pro.nvim',
-  config = function()
-    require('monokai-pro').setup {
-      filter = 'spectrum',
-    }
-    vim.cmd [[colorscheme monokai-pro]]
-  end,
-}
